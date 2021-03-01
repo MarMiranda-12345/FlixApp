2c45c50f5f5192e2b74619baea0b5869b9e53ae5
sorryForDisappointing <53985598+sorryForDisappointing@users.noreply.github.com>
2021-03-01 12:39:28 -0800

    Add files via upload

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Mariano Miranda Herrera on 2/26/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Mariano Miranda Herrera on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
